e447eefaba1f5cb44b69151d161c0f3623759026
Talon Hughes <talon.hughes@gmail.com>
2017-06-15 18:26:42 -0600

    testing the port for digital ocean

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = module.exports = express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 80;
 
 // Serve files
 app.use(express.static(__dirname ));
