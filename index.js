94ca2ec07d7d45a55aa681a74e382e81ecff0603
Talon Hughes <talon.hughes@gmail.com>
2017-06-15 21:15:17 -0600

    changed file structure

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const app = module.exports = express();
-const port = process.env.PORT || 80;
+const app = express();
+const port = process.env.PORT || 8080;
 
 // Serve files
-app.use(express.static(__dirname ));
+app.use(express.static(__dirname + '/public'));
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
