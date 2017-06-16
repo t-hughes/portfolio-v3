e177dcd9ad0000f8a31938a80f0241d2567fbced
Talon Hughes <talon.hughes@gmail.com>
2017-06-16 15:13:18 -0600

    changing port to work with unbuntu server

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 8080;
 
 // Serve files
 app.use(express.static(__dirname + '/public'));
