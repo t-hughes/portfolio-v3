1f0d4da7703f930ee071bd290bb14f096e13ac7c
Talon Hughes <talon.hughes@gmail.com>
2017-06-16 15:42:24 -0600

    removed env var for the moment

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 8080;
+const port = 8080;
 
 // Serve files
 app.use(express.static(__dirname + '/public'));
